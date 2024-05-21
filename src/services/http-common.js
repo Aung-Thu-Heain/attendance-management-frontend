47e8493845ee59e4ca1007f35634ac243a66b1ab
david-dev-23711 <david@leaptechnology.com.sg>
2024-05-22 00:11:35 +0630

    setup-axios

---
+++ src/services/http-common.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+const baseUrl = import.meta.env.VITE_BASE_URL;
+
+axios.defaults.baseURL = baseUrl;
+
+axios.interceptors.request.use((config) => {
+  // Default Content-Type
+  let contentType = "application/json";
+
+  // Check if the config specifies a different content type
+  if (config.headers && config.headers["Custom-Content-Type"]) {
+    contentType = config.headers["Custom-Content-Type"];
+    delete config.headers["Custom-Content-Type"];
+  }
+  config.headers["Content-Type"] = contentType;
+  config.headers["Accept"] = "application/json";
+  config.headers["Authorization"] = "";
+
+  return config;
+});
+
+//For Unauthorized response handler
+axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export const HTTP = axios;
