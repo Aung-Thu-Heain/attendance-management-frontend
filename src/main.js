8a7d2afe814c793ae7be14f44720a56adde24059
david-dev-23711 <david@leaptechnology.com.sg>
2024-05-21 23:54:47 +0630

    first-commit

---
+++ src/main.js
@@ -0,0 +1,24 @@
+/**
+ * main.js
+ *
+ * Bootstraps Vuetify and other plugins then mounts the App`
+ */
+
+// Plugins
+import { registerPlugins } from "@/plugins";
+import { createPinia } from "pinia";
+
+// Components
+import App from "./App.vue";
+
+// Composables
+import { createApp } from "vue";
+import router from "./router";
+
+const app = createApp(App);
+const pinia = createPinia();
+
+registerPlugins(app);
+app.use(router);
+app.use(pinia);
+app.mount("#app");
